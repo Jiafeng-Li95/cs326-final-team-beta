583c47e6f87d3b8203cadf537ee576c387d7bb19
yongchangcheng <cyc137316@gmail.com>
2021-10-21 23:50:02 -0400

    update

---
+++ front-end/vendor/vendor.js
@@ -0,0 +1,7 @@
+function addToCart(){
+    alert("Add to cart!");
+}
+
+document.images[2].addEventListener('click', addToCart);
+document.images[4].addEventListener('click', addToCart);
+document.images[6].addEventListener('click', addToCart);
