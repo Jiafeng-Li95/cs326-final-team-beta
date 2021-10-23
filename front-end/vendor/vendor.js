cf5d42857ca173fd29f6b7551954663ff53f5e5c
yongchangcheng <cyc137316@gmail.com>
2021-10-23 01:01:49 -0400

    update

--- front-end/vendor/vendor.js
+++ front-end/vendor/vendor.js
@@ -2,27 +2,6 @@ function addToCart(){
     alert("Add to cart!");
 }
 
-function account(){
-    
-}
-
-function addProduct(){
-
-}
-
-function help(){
-
-}
-
-function signOut(){
-
-}
-
-
-document.getElementById("account").addEventListener("click", account);
-document.getElementById("add").addEventListener("click", addProduct);
-document.getElementById("help").addEventListener("click", help);
-document.getElementById("signout").addEventListener("click", signOut);
 const buttons = document.getElementsByClassName("btn btn-outline-light");
 for (let button of buttons){
     button.addEventListener("click", addToCart);
