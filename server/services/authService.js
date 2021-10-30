a0dcfbf4b7c3d545235eba1e997c6fc7c0bc97c2
Qiming-C <49624964+Qiming-C@users.noreply.github.com>
2021-10-29 23:09:54 -0400

    added get product endpoint

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,3 +1,4 @@
 //put this at end of the this file
 //remember add the function inside this {}
+
 module.exports = {};
--- server/services/productService.js
+++ server/services/productService.js
@@ -1,3 +1,24 @@
 //put this at end of the this file
 //remember add the function inside this {}
-module.exports = {};
+let faker = require("faker");
+
+//fake datas
+let products = new Array(15).fill(null).map((product, index) => {
+  return (product = {
+    id: faker.datatype.number(),
+    name: faker.animal.fish(),
+    description: faker.lorem.words(),
+    userId: index,
+  });
+});
+
+/**
+ *
+ * @param {*} vendorId number
+ * @returns all products by specified vendor
+ */
+function getAllProductsByVendor(vendorId) {
+  return products.filter((product) => product.userId === vendorId);
+}
+
+module.exports = { getAllProductsByVendor };
