ab83402814548a303b6b8205731986a8fcc577b5
Qiming-C <49624964+Qiming-C@users.noreply.github.com>
2021-11-03 16:53:48 -0400

    added more mocked-data

--- server/faker_data.js
+++ server/faker_data.js
@@ -6,11 +6,41 @@ let vendors = new Array(15).fill(null).map((vendor, index) => {
     username: faker.name.firstName(),
     password: faker.datatype.number(),
     name: faker.company.companyName(),
-    description: faker.lorem.words(),
+    description: faker.lorem.paragraphs(),
     location: faker.address.cityName(),
     phoneNumber: faker.phone.phoneNumber(),
     userId: index,
   });
 });
+
+vendors.push(
+  {
+    username: "jimmy",
+    password: "123",
+    name: faker.company.companyName(),
+    description: faker.lorem.paragraphs(),
+    location: faker.address.cityName(),
+    phoneNumber: faker.phone.phoneNumber(),
+    userId: 999,
+  },
+  {
+    username: "jeff",
+    password: "123",
+    name: faker.company.companyName(),
+    description: faker.lorem.paragraphs(),
+    location: faker.address.cityName(),
+    phoneNumber: faker.phone.phoneNumber(),
+    userId: 9999,
+  },
+  {
+    username: "yongchang",
+    password: "123",
+    name: faker.company.companyName(),
+    description: faker.lorem.paragraphs(),
+    location: faker.address.cityName(),
+    phoneNumber: faker.phone.phoneNumber(),
+    userId: 99999,
+  }
+);
 
 module.exports = vendors;
