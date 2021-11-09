16c9b73b75b92bd2b0385b3bd148f08079d2797d
Jiafeng-Li95 <jeff1106593594@gmail.com>
2021-11-09 13:45:54 -0500

    update the auth login
    auth login has been changed by using database.

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -4,34 +4,22 @@ const authService = require("../services/authService");
 
 //router for sign in
 authRouter.post("/login", async (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
 
-  let user = authService.checkLoginExist(password, username);
+  let user = await authService.loginAuth(req.body.password, req.body.username);
 
-  user ? res.status(200).json(user)
+  user ? res.status(200).json({ message: "User logged in successfully" })
     : res.status(401).json({ message: "Failed login has happened" });
 });
 
 //router for sign up a new user 
 authRouter.post("/signup", async (req, res) => {
-  let username = req.body.username;
-  // authService.checkNameExist(username).then((user) => {
-  //   console.log(user);
-  // });
 
-  // if (authService.checkNameExist(username)) {
-  //   return res.status(401).json({ message: "Unauthorized client error status" });
-  // }
-  // else {
-  let signupStatus = await authService.createUser(req.body.username, req.body.password, req.body.name,
+  let signup = await authService.createUser(req.body.username, req.body.password, req.body.name,
     req.body.description, req.body.location, req.body.phoneNumber, req.body.isVendor);
 
-  signupStatus
+  signup
     ? res.status(200).json({ message: "User created successfully" })
-    : res.status(401).json({ message: "Failed login has happened" });
-  // return res.status(200).json({ message: "Created a new user" });
-  // }
+    : res.status(401).json({ message: "User fail to create" });
 });
 
 //end of file
--- server/services/authService.js
+++ server/services/authService.js
@@ -9,64 +9,37 @@ const UserRepository = require("../model/user");
 // //init the product repo
 const userRepository = new UserRepository(db);
 
-const userInfo = require("../faker_data");
-let userId = 0;
-
-// async function checkNameExist(username) {
-//   // let checkNameExist = false;
-//   // console.log(userRepository.findProductsByUsername(username));
-
-//   // userInfo.forEach((e) => {
-//   //   checkNameExist = e.username === username ? true : false;
-//   // });
-//   // if (checkNameExist) return true;
-//   // else return false;
-//   return await userRepository.findProductsByUsername(username);
-// }
-
-function checkLoginExist(password, username) {
-  let checkPassExist = false;
-  let checkNameExist = false;
-  userInfo.every((e) => {
-    checkPassExist = parseInt(e.password) === parseInt(password) ? true : false;
-    checkNameExist = e.username === username ? true : false;
-    if (checkPassExist && checkNameExist) {
-      userId = e.userId;
+//login authentication
+async function loginAuth(password, username) {
+  try {
+    let userInfo = await userRepository.findProductsByUsername(username);
+    //No username is in database
+    if (Object.keys(userInfo).length === 0) {
       return false;
     }
-    return true;
-  });
-  if (checkPassExist && checkNameExist) return { userId: userId };
-  else return false;
-}
-
-// add new user to the database
-async function createUser(
-  username,
-  password,
-  name,
-  description,
-  location,
-  phonenumber,
-  isVendor
-) {
-  try {
-    await userRepository.addUser(
-      username,
-      password,
-      name,
-      description,
-      location,
-      phonenumber,
-      isVendor
-    );
+    else {
+      if (userInfo[0].password !== password) {
+        return false;
+      }
+    }
   } catch (error) {
     return false;
   }
   return true;
 }
 
+// add new user to the database
+async function createUser(username, password, name, description, location, phonenumber, isVendor) {
+  try {
+    await userRepository.addUser(username, password, name, description, location, phonenumber, isVendor);
+  }
+  catch (error) {
+    return false;
+  }
+  return true;
+}
+
 module.exports = {
-  checkLoginExist,
+  loginAuth,
   createUser,
 };
