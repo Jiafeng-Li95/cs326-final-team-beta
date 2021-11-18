326a393de1b68249e72295de888786de1cc9a47e
yongchangcheng <cyc137316@gmail.com>
2021-11-18 14:44:38 -0500

    add

---
+++ server/model/viewed.js
@@ -0,0 +1,47 @@
+class ViewRepository {
+    constructor(db) {
+        this.db = db;
+        this.createTable();
+    }
+
+    //create table
+    async createTable() {
+        return await this.db.none(`CREATE TABLE IF NOT EXISTS viewinfo(
+          userid int PRIMARY KEY,
+          numclicked int not null
+      )`);
+    }
+
+    async dropTable() {
+        return await this.db.none(`DROP TABLE user`);
+    }
+
+    //create a new user Information
+    async addUser(userid) {
+        return await this.db.none(
+            "INSERT INTO userInfo (userid, numclicked) VALUES(${userid},${numclicked})",
+            {
+                userid: userid,
+                numclicked: 0
+            }
+        );
+    }
+
+    //return user Information by speicifed id
+    async getNumClickedById(id) {
+        return await this.db.oneOrNone(
+            "SELECT * FROM viewinfo WHERE id = $1",
+            id
+        );
+    }
+
+    //update user information(For vendor)
+    async updateViewInfo(numclicked, id) {
+        return await this.db.none(
+            "UPDATE viewinfo SET numclicked = $1 WHERE id = $2",
+            [numclicked+1, id]
+        );
+    }
+}
+
+module.exports = ViewRepository;
